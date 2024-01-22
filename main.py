209eebd2742ddc6c8e5ca99bce1e61c63ea2521d
NHirt32 <nicholas.t.hirt@gmail.com>
2024-01-22 14:33:43 -0800

    No longer droping categorical data; instead attempting boolean conversion based on first characters in applicable columns. However, facing an issue where features present in training data are not present in testing data (Cabin_T, Ticket_5, Ticket_8). Applied min-max feature scaling to: PassengerId, Pclass, Age, SibSp, Parch, Fare

--- main.py
+++ main.py
@@ -12,27 +12,41 @@ naiveBayes = GaussianNB()
 
 # Dropping irrelevant column 'Name'
 trainingData.drop("Name", axis=1, inplace=True)
+testData.drop("Name", axis=1, inplace=True)
 # Normalizing Sex to 0(male) or 1(female)
 trainingData['Sex'] = trainingData['Sex'].map({'male': 0, 'female': 1})
-
-# Temp drop on categorical data
-trainingData.drop(['Ticket', 'Cabin', 'Embarked'], axis=1, inplace=True)
-trainingData.ffill(inplace=True)
-# Temp processing of test data
-testData.drop("Name", axis=1, inplace=True)
 testData['Sex'] = testData['Sex'].map({'male': 0, 'female': 1})
-testData.drop(['Ticket', 'Cabin', 'Embarked'], axis=1, inplace=True)
+# Replacing all values in categorical columns with first character of each value if value is a non-empty string
+trainingData['Cabin'] = trainingData['Cabin'].apply(lambda x: x[0] if isinstance(x,str) and len(x) > 0 else x)
+testData['Cabin'] = testData['Cabin'].apply(lambda x: x[0] if isinstance(x,str) and len(x) > 0 else x)
+trainingData['Ticket'] = trainingData['Ticket'].apply(lambda x: x[0] if isinstance(x,str) and len(x) > 0 else x)
+testData['Ticket'] = testData['Ticket'].apply(lambda x: x[0] if isinstance(x,str) and len(x) > 0 else x)
+
+# Filling missing data in columns
+trainingData.ffill(inplace=True)
 testData.ffill(inplace=True)
 
 # Replacing categorical columns with new binary columns
-# trainingData = pd.get_dummies(trainingData, columns=['Ticket', 'Cabin', 'Embarked'])
+trainingData = pd.get_dummies(trainingData, columns=['Ticket', 'Cabin', 'Embarked'])
+testData = pd.get_dummies(testData, columns=['Ticket', 'Cabin', 'Embarked'])
+
+# Applying min-max feature scaling to the following columns: PassengerId, Pclass, Age, SibSp, Parch, Fare
+scalingFeatureList = ['PassengerId', 'Pclass', 'Age', 'SibSp', 'Parch', 'Fare']
+for feature in scalingFeatureList:
+    trainingMax = trainingData[feature].max()
+    testMax = testData[feature].max()
+    trainingMin = trainingData[feature].min()
+    testMin = testData[feature].min()
+    trainingData[feature] = (trainingData[feature]-trainingMin)/(trainingMax-trainingMin)
+    testData[feature] = (testData[feature] - testMin) / (testMax - testMin)
 
 
 # Creating an X and Y to pass into NaiveBayes algorithm
 features = trainingData.drop("Survived", axis=1)
 target = trainingData[['Survived']]
 
-# features = features.astype('float32')
+# Converting boolean binary categories to numeric
+features = features.astype('float32')
 
 print(features.head())
 
