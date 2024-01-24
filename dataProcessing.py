bd3903bbc327f6a614fc74ffc9be98f8681d98d8
NHirt32 <nicholas.t.hirt@gmail.com>
2024-01-24 13:46:07 -0800

    Modularized code moving functionality from main to a data processing file. Currently still need to extract titles from Name column. Other than this Data Processing is completed.

---
+++ dataProcessing.py
@@ -0,0 +1,92 @@
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+
+
+nonNumericColumns = ['Cabin', 'Ticket', 'Embarked', 'Sex']
+missingValueColumns = ['Age', 'Cabin', 'Embarked']
+scalableFeatureList = ['PassengerId', 'Pclass', 'Age', 'SibSp', 'Parch', 'Fare', 'Cabin', 'Embarked', 'Ticket']
+
+def fileRead(trainFile, testFile):
+    """
+    This function converts two .csv files into pandas dataframes
+
+    Args:
+        trainFile (.csv): Relative file path of training .csv file
+        testFile (.csv): Relative file path of testing .csv file
+    Returns:
+        tuple: Returns a tuple containing both .csv files transformed into pandas dataframes
+    """
+    trainingData = pd.read_csv(trainFile)
+    testData = pd.read_csv(testFile)
+    return trainingData, testData
+
+def encoding(trainingData, testData):
+    """
+    This function encodes non-numeric columns of two dataframes into numeric columns
+
+    Args:
+        trainingData (DataFrame): Training data of machine learning model in Pandas DataFrame format
+        testData (DataFrame): Test Data for machine learning model in Pandas DataFrame format
+    Returns:
+        tuple: Returns a tuple of training and testing dataframes with non-numeric data encoded into numeric data
+    """
+    le = LabelEncoder()
+    for column in nonNumericColumns:
+        trainingData[column] = le.fit_transform(trainingData[column])
+        testData[column] = le.fit_transform(testData[column])
+    return trainingData, testData
+
+def impute(trainingData, testData):
+    """
+    This function imputes missing column values of our machine learning dataframes with medians of said columns
+
+    Args:
+        trainingData (DataFrame): Training data of machine learning model in Pandas DataFrame format
+        testData (DataFrame): Test Data for machine learning model in Pandas DataFrame format
+    Returns:
+        tuple: Returns a tuple of training and testing dataframes where
+               all missing values in columns have been imputed with the existing median of the column
+    """
+    for column in missingValueColumns:
+        columnMean = trainingData[column].mean()
+        trainingData.fillna({column: columnMean}, inplace=True)
+        columnMean = testData[column].mean()
+        testData.fillna({column: columnMean}, inplace=True)
+    return trainingData, testData
+
+def scaling(trainingData, testData):
+    """
+    This function utilizes min-max scaling to scale all numeric values in our two dataframes
+
+    Args:
+        trainingData (DataFrame): Training data of machine learning model in Pandas DataFrame format
+        testData (DataFrame): Test Data for machine learning model in Pandas DataFrame format
+    Returns:
+        tuple: Returns a tuple of training and testing dataframes where all numeric columns have been scaled between 0 - 1
+    """
+    for feature in scalableFeatureList:
+        trainingMax = trainingData[feature].max()
+        testMax = testData[feature].max()
+        trainingMin = trainingData[feature].min()
+        testMin = testData[feature].min()
+        trainingData[feature] = (trainingData[feature] - trainingMin) / (trainingMax - trainingMin)
+        testData[feature] = (testData[feature] - testMin) / (testMax - testMin)
+    return trainingData, testData
+
+def dataPreprocessing(trainingFile, testingFile):
+    """
+    This function utilizes various other dataprocessing functions the ordering of this processing is: Reading the
+    data into pandas DataFrames -> Encoding non-numeric values -> imputing missing values -> min-max scaling numeric
+    values
+
+    Args:
+        trainingFile (.csv): .csv file containing our ML-model's training data
+        testingFile (.csv): .csv file containing our ML-model's training data
+    Returns:
+        tuple: Returns two DataFrames which have been encoded, imputed, and scaled
+    """
+    trainingData, testData = fileRead(trainingFile, testingFile)
+    trainingData, testData = encoding(trainingData, testData)
+    trainingData, testData = impute(trainingData, testData)
+    trainingData, testData = scaling(trainingData, testData)
+    return trainingData, testData
--- main.py
+++ main.py
@@ -1,74 +1,28 @@
 import pandas as pd
-import numpy as np
 from sklearn.naive_bayes import GaussianNB
-from sklearn.preprocessing import LabelEncoder # For replacing non-numeric data with numeric data
-from sklearn.impute import SimpleImputer # For filling missing values with mean
-from sklearn.compose import ColumnTransformer # For applying encoder to dataframe columns
+import dataProcessing as dp
 
 # Removing the max column account to see all model features
 pd.set_option('display.max_columns', None)
 
-trainingData = pd.read_csv('train.csv')
-testData = pd.read_csv('test.csv')
 
-naiveBayes = GaussianNB()
-
-# Checking dataframe properties to scan for NaN values
-print(trainingData.info())
-# NaN values in Age, Cabin, and Embarked.
-# Creating encoder to replace non-numeric values with numeric values
-le = LabelEncoder()
-nonNumeric = ['Cabin', 'Ticket', '']
-trainingData['Cabin'] = le.fit_transform(trainingData['Cabin'])
-
-# Dropping irrelevant column 'Name', in the future we will apply regex's to extract Titles
-trainingData.drop("Name", axis=1, inplace=True)
-testData.drop("Name", axis=1, inplace=True)
-# Normalizing Sex to 0(male) or 1(female)
-trainingData['Sex'] = trainingData['Sex'].map({'male': 0, 'female': 1})
-testData['Sex'] = testData['Sex'].map({'male': 0, 'female': 1})
-# Replacing all values in categorical columns with first character of each value if value is a non-empty string
-# trainingData['Cabin'] = trainingData['Cabin'].apply(lambda x: x[0] if isinstance(x,str) and len(x) > 0 else x)
-# testData['Cabin'] = testData['Cabin'].apply(lambda x: x[0] if isinstance(x,str) and len(x) > 0 else x)
-# trainingData['Ticket'] = trainingData['Ticket'].apply(lambda x: x[0] if isinstance(x,str) and len(x) > 0 else x)
-# testData['Ticket'] = testData['Ticket'].apply(lambda x: x[0] if isinstance(x,str) and len(x) > 0 else x)
+# Data processing
+trainingData, testData = dp.dataPreprocessing("train.csv", "test.csv")
 
 
-# Filling missing data in columns
-trainingData.ffill(inplace=True)
-testData.ffill(inplace=True)
-
-# Replacing categorical columns with new binary columns
-# trainingData = pd.get_dummies(trainingData, columns=['Ticket', 'Cabin', 'Embarked'])
-testData = pd.get_dummies(testData, columns=['Ticket', 'Cabin', 'Embarked'])
-
-# Applying min-max feature scaling to the following columns: PassengerId, Pclass, Age, SibSp, Parch, Fare
-scalingFeatureList = ['PassengerId', 'Pclass', 'Age', 'SibSp', 'Parch', 'Fare']
-for feature in scalingFeatureList:
-    trainingMax = trainingData[feature].max()
-    testMax = testData[feature].max()
-    trainingMin = trainingData[feature].min()
-    testMin = testData[feature].min()
-    trainingData[feature] = (trainingData[feature]-trainingMin)/(trainingMax-trainingMin)
-    testData[feature] = (testData[feature] - testMin) / (testMax - testMin)
-
-
+# Machine Learning
 # Creating an X and Y to pass into NaiveBayes algorithm
 features = trainingData.drop("Survived", axis=1)
 target = trainingData[['Survived']]
-
-# Converting boolean binary categories to numeric (For get_dummies, may drop)
-# features = features.astype('float32')
-
 print(features.head())
 
-# Machine Learning
-
+naiveBayes = GaussianNB()
 # naiveBayes.fit(features, target.values.ravel())
 # survPrediction = naiveBayes.predict(testData)
 #
 # prediction = pd.DataFrame({'PassengerId':testData.PassengerId, 'Survived':survPrediction})
 # print(prediction.head())
-
+# Current Questions:
 # What is SibSp, Parch, and Pclass
+# How is Feature Engineering(Categorization) affected by min-max feature scaling?
 
